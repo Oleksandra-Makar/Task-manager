9ab3e0793a483156d27be30cffd7a33a672fd41d
Олександра Макар <oleksandra.201153@gmail.com>
2021-02-25 00:58:42 +0200

    Updated sorting data with different options

--- src/routers/task.js
+++ src/routers/task.js
@@ -20,23 +20,31 @@ router.post('/tasks', auth, async (req, res) => {
 
 // GET /tasks?/completed=true
 // GET /tasks?limit=10&skip=0
+// GET /tasks?sortBy=createdA:desc
 router.get('/tasks', auth, async (req, res) => {
-    const match = {
+    const match = {};
+    const sort = {};
 
-    }
     if (req.query.completed) {
-        match.completed = req.query.completed === 'true'
+        match.completed = req.query.completed === 'true';
     }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':');
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
+    }
+
     try{
         await req.user.populate({
             path: 'tasks',
             match, 
             options: {
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
             }
         }).execPopulate();
-        res.send(req.user.tasks)
+        res.send(req.user.tasks);
     } catch (e) {
         res.send(500).send(e);
     }
@@ -51,7 +59,7 @@ router.get('/tasks/:id', auth, async (req, res) => {
         if (!task) {
            return res.status(404).send();
         }
-        res.send(task)
+        res.send(task);
     } catch (e) {
         res.status(500).send(e);
     }
