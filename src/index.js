1c6665644448d91a0eeac8e14e74f5245e3be2d4
Олександра Макар <oleksandra.201153@gmail.com>
2021-02-28 20:52:04 +0200

    Added ability to upload avatar and delete it

--- src/index.js
+++ src/index.js
@@ -3,13 +3,8 @@ require('./db/mongoose');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 
-
 const app = express();
 const port = process.env.PORT || 3000;
-
-// app.use((req, res, next) => {
-//     res.status(503).send('Site is currently down. Check back soon');
-// })
 
 app.use(express.json());
 app.use(userRouter);
@@ -18,15 +13,3 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
 });
-
-const Task = require('./models/task');
-const User = require('./models/user');
-
-const main = async () => {
-
-    const user = await User.findById('6036cd109224515364d3be83');
-    await user.populate('tasks').execPopulate();
-    console.log(user.tasks);
-}
-
-main()
